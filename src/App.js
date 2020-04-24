f1b54a7afd3ee5a7ffc47caad55ed12e2766698d
Revanth-M <revanthkumar.makkena@gmail.com>
2020-04-24 15:45:05 -0700

    WIP - React Playground - Added Project (React) - Created components - Out of those, 1 has state hook to set counter - In order to accomodate multiple components in the root, we can use React Fragment (<> </>) - But the easiest way would be to create another compenent and export that to the root

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [counter, setCounter] = useState(5);
+  const handleClick = () => setCounter(counter + 1);
+  return <button onClick={handleClick}> {counter} </button>; //React.createElement("button", null, "{ counter }");
 }
 
 export default App;
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import App from "./App";
+import Display from "./display";
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      <App></App>
+      <Display></Display>
+    </div>
+  );
+}
+export default Main;
