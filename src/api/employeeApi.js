d8e35a841a4ec345dc202c99a006d6b4945ed1bd
Revanth-M <revanthkumar.makkena@gmail.com>
2020-05-23 11:22:17 -0700

    + Backend Server configuration + Data

---
+++ src/api/employeeApi.js
@@ -0,0 +1,22 @@
+import {handleResponse, handleError} from "./apiUtils";
+const baseUrl = process.env.REACT_APP_API_URL + "/employees/";
+
+export function getEmployees() {
+  return fetch(baseUrl).then(handleResponse).catch(handleError);
+}
+
+export function saveEmployee(employee) {
+  return fetch(baseUrl + (employee.id || ""), {
+    method: employee.id ? "PUT" : "POST", // POST for create, PUT to update when id already exists.
+    headers: {"content-type": "application/json"},
+    body: JSON.stringify(employee),
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function deleteEmployee(employeeId) {
+  return fetch(baseUrl + employeeId, {method: "DELETE"})
+    .then(handleResponse)
+    .catch(handleError);
+}
