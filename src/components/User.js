8930acd955b86d654d6b63be20f85f86c10c2538
Revanth-M <revanthkumar.makkena@gmail.com>
2020-05-24 21:36:46 -0700

    Decreased records cause of system overhead

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {getUsers} from "../api/userApi";
+import {getCourses} from "../api/courseApi";
 
 export default function User() {
-  return <p>M user</p>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers((_users) => setUsers(_users));
+  }, [users]);
+
+  return (
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author ID</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => {
+            return (
+              <tr key={user.First_Name}>
+                <td>{user.Last_Name}</td>
+                <td>{user.Gender}</td>
+                <td>{user.Race}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
