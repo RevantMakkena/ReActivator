99262b326a946dd01003240a7c1450bf89b41919
Revanth-M <revanthkumar.makkena@gmail.com>
2020-06-01 20:47:59 -0500

    Restructring Project

--- src/FuncComponentEx/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./App.css";
-
-function App(props) {
-  const handleClick = () => props.setValue(props.incVal);
-  return <button onClick={handleClick}> +{props.incVal} </button>;
-}
-
-export default App;
--- src/GitProfileSearch/TestData.js
+++
@@ -1,8 +0,0 @@
-const testData = [
-  {name: "Revanth", company: "EO"},
-  {name: "kumar", company: "N/A"},
-  {name: "kalyan", company: "Kent State"},
-  {name: "Madhav", company: "Detroit state"},
-];
-
-export default testData;
