5e58b9e861e225d3a25332eeb50d793d579dd72a
Revanth-M <revanthkumar.makkena@gmail.com>
2020-06-02 15:05:02 -0500

    Notes APP

---
+++ Projects/notes/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import AddNotes from "./components/AddNotes";
+
+function App() {
+  return (
+    <div className='App'>
+      <AddNotes />
+    </div>
+  );
+}
+
+export default App;
---
+++ Projects/notes/src/components/AddNotes.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddNotes = () => {
+  return <h4>Add Notes</h4>;
+};
+
+export default AddNotes;
