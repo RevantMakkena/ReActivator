88331c5401ce56a1a0b8f7f220c10dece3200436
Revanth-M <revanthkumar.makkena@gmail.com>
2020-06-02 15:48:07 -0500

    Added Notes and rendering a diff component for list of notes

--- Projects/notes/src/components/AddNotes.js
+++ Projects/notes/src/components/AddNotes.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
+import {SnackbarContent, TextField, Button} from "@material-ui/core";
 
 const AddNotes = () => {
-  return <h4>Add Notes</h4>;
+  const [notes, setNotes] = useState([]);
+  return (
+    <div>
+      <div className='row'>
+        <TextField id='outlined-basic' label='Notes' />
+      </div>
+      <div className='row'>
+        <Button variant='contained' color='primary'>
+          Add Notes
+        </Button>
+      </div>
+      <div
+        className='row'
+        style={{
+          marginLeft: "100px",
+          marginRight: "100px",
+        }}>
+        <NoteComponent currentNote={notes} />
+      </div>
+    </div>
+  );
+};
+
+const NoteComponent = (props) => {
+  return (
+    <div>
+      {props.notes
+        ? props.notes.map((currentNote) => {
+            <SnackbarContent
+              message={currentNote}
+              style={{marginBottom: "10px"}}></SnackbarContent>;
+          })
+        : ""}
+    </div>
+  );
 };
 
 export default AddNotes;
