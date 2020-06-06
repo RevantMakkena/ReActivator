ab94489a6135ac32cf0cd68a9fbe0af0c9406048
Revanth-M <revanthkumar.makkena@gmail.com>
2020-06-05 20:58:19 -0500

    Finished Notes

--- Projects/notes/src/components/AddNotes.js
+++ Projects/notes/src/components/AddNotes.js
@@ -1,19 +1,31 @@
 import React, {useState, useEffect} from "react";
-import {SnackbarContent, TextField, Button} from "@material-ui/core";
+import {
+  SnackbarContent,
+  TextField,
+  Button,
+  Grid,
+} from "@material-ui/core";
 
 const AddNotes = () => {
   const [notes, setNotes] = useState([]);
   const [input, setInput] = useState("");
 
   useEffect(() => {}, [notes]);
+  let noteCount = 1;
 
   const updateInput = (e) => {
     setInput(e.target.value);
   };
 
   const updateNotes = () => {
-    setNotes((_notes) => [..._notes, input]);
+    if (input.length === 0) return alert("Please enter notes");
+    setNotes((_notes) => [..._notes, {id: noteCount, note: input}]);
+    noteCount = noteCount + 1;
     setInput("");
+  };
+
+  const deleteNotes = (e) => {
+    setNotes(notes.filter((_note) => _note.id !== e));
   };
 
   return (
@@ -40,21 +52,42 @@ const AddNotes = () => {
           marginLeft: "100px",
           marginRight: "100px",
         }}>
-        <NoteComponent currentNote={notes} />
+        <NoteComponent currentNote={notes} delete={deleteNotes} />
       </div>
     </div>
   );
 };
 
 const NoteComponent = (props) => {
+  function removeNote(e) {
+    props.delete(e);
+  }
   return (
     <div>
       {props.currentNote
         ? props.currentNote.map((_currentNote) => {
             return (
-              <SnackbarContent
-                message={_currentNote}
-                style={{marginBottom: "10px"}}></SnackbarContent>
+              <>
+                <Grid container>
+                  <Grid sm={9}>
+                    <SnackbarContent
+                      message={_currentNote.note}
+                      style={{
+                        marginBottom: "10px",
+                      }}></SnackbarContent>
+                  </Grid>
+                  <Grid sm={3}>
+                    <Button
+                      variant='outlined'
+                      color='secondary'
+                      onClick={() => {
+                        removeNote(_currentNote.id);
+                      }}>
+                      Delete
+                    </Button>
+                  </Grid>
+                </Grid>
+              </>
             );
           })
         : ""}
