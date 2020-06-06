4dbf3a63cc03f44d5bec6363c0cd8a23cc910f86
Revanth-M <revanthkumar.makkena@gmail.com>
2020-06-06 15:45:40 -0500

    Used useForm() Hook

--- Projects/Forms/simpleform/src/App.js
+++ Projects/Forms/simpleform/src/App.js
@@ -4,22 +4,37 @@ import Form from "./form";
 import "react-toastify/dist/ReactToastify.css";
 import {BrowserRouter, Route, Switch, Link} from "react-router-dom";
 import UseFormHookEx from "./UseFormHookExample";
+import {Container} from "@material-ui/core";
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
-        <Link to='/'>Form</Link> {" | "}
+        <Link to='/'>Home</Link> {" | "}
+        <Link to='/ComplicatedForm'>Form</Link> {" | "}
         <Link to='/SimplifiedForm'>Simplified Form</Link>
         <Switch>
           <Route
             path='/SimplifiedForm'
             component={UseFormHookEx}></Route>
-          <Route path='/' component={Form}></Route>
+          <Route path='/ComplicatedForm' component={Form}></Route>
+          <Route path='/' component={Home}></Route>
         </Switch>
       </BrowserRouter>
     </div>
   );
 }
+
+const Home = () => {
+  return (
+    <>
+      <Container maxWidth='sm' style={{marginTop: "100px"}}>
+        <i
+          className='fas fa-address-card'
+          style={{fontSize: "350px", color: "olive"}}></i>
+      </Container>
+    </>
+  );
+};
 
 export default App;
--- Projects/Forms/simpleform/src/UseFormHookExample.js
+++ Projects/Forms/simpleform/src/UseFormHookExample.js
@@ -1,7 +1,104 @@
-import React from "react";
+import React, {useState} from "react";
+import {useForm} from "react-hook-form";
+import {
+  TextField,
+  makeStyles,
+  Button,
+  Container,
+} from "@material-ui/core";
+import {ToastContainer, toast} from "react-toastify";
+import "./App.css";
+
+const useStyles = makeStyles((theme) => ({
+  fields: {
+    marginTop: "20px",
+    width: "100%",
+  },
+  submitFields: {
+    marginTop: "10px",
+    width: "50%",
+  },
+}));
 
 const UseFormHookEx = () => {
-  return <div>Use Form Hook</div>;
+  const classes = useStyles();
+  const {register, handleSubmit, watch, errors} = useForm();
+
+  const onSubmit = (e) => {
+    toast.success("Data is successfully submitted!!", {
+      position: toast.POSITION.TOP_LEFT,
+    });
+    return false;
+  };
+  return (
+    <>
+      <ToastContainer />
+      <Container maxWidth='sm'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <TextField
+              className={classes.fields}
+              variant='outlined'
+              label='User Name'
+              name='username'
+              inputRef={register({required: true, maxLength: 5})}
+            />
+            {errors.username && <p>Username is required</p>}
+          </div>
+          <div>
+            <TextField
+              className={classes.fields}
+              variant='outlined'
+              label='First Name'
+              name='firstname'
+              inputRef={register({required: true, maxLength: 5})}
+            />
+            {errors.firstname && <p>First Name is required</p>}
+          </div>
+          <div>
+            <TextField
+              className={classes.fields}
+              variant='outlined'
+              label='Last Name'
+              name='lastname'
+              inputRef={register({required: true, maxLength: 5})}
+            />
+            {errors.lastname && <p>Last Name is required</p>}
+          </div>
+          <div>
+            <TextField
+              className={classes.fields}
+              variant='outlined'
+              label='Email'
+              name='email'
+              inputRef={register({required: true, maxLength: 5})}
+            />
+            {errors.email && <p>Email is required</p>}
+          </div>
+          <div>
+            <TextField
+              className={classes.fields}
+              variant='outlined'
+              label='Password'
+              type='password'
+              name='password'
+              inputRef={register({required: true, maxLength: 10})}
+            />
+            {errors.password && <p>Password is required</p>}
+          </div>
+          <div>
+            <Button
+              type='submit'
+              variant='contained'
+              color='primary'
+              className={classes.submitFields}>
+              Submit
+            </Button>
+          </div>
+        </form>
+      </Container>
+    </>
+  );
 };
 
 export default UseFormHookEx;
