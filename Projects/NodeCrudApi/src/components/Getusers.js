d912296941ebd9da93c692bc4e7e60999a783ba9
Revanth-M <revanthkumar.makkena@gmail.com>
2020-06-24 21:38:33 -0500

    Added Get ALL

--- Projects/NodeCrudApi/src/components/Getusers.js
+++ Projects/NodeCrudApi/src/components/Getusers.js
@@ -1,9 +1,22 @@
 import React, {useState, useEffect} from "react";
 
 import {getAll} from "./UsersApi.js";
+import {TableComponent} from "./TableComponent";
 
 export const GetUsers = () => {
   const [users, setUsers] = useState([]);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [page, setPage] = React.useState(0);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   useEffect(() => {
     const fetchUsers = async () => {
       const res = await getAll();
@@ -14,17 +27,17 @@ export const GetUsers = () => {
 
   return (
     <>
-      {users
-        ? users.map((user) => {
-            return (
-              <table>
-                <tbody>
-                  <tr>{JSON.stringify(user)}</tr>
-                </tbody>
-              </table>
-            );
-          })
-        : ""}
+      {users ? (
+        <TableComponent
+          users={users}
+          handleChangePage={handleChangePage}
+          handleChangeRowsPerPage={handleChangeRowsPerPage}
+          page={page}
+          rowsPerPage={rowsPerPage}
+        />
+      ) : (
+        ""
+      )}
     </>
   );
 };
