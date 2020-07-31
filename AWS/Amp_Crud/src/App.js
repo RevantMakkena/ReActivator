0b1cce45d29883d94ffae57d855ab3ff1895ba12
Revanth-M <revanthkumar.makkena@gmail.com>
2020-07-30 20:42:16 -0500

    Amplify API + Lambda

--- AWS/Amp_Crud/src/App.js
+++ AWS/Amp_Crud/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {API} from "aws-amplify";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  async function callApi() {
+    const people = API.get("CRUDApi", "/people");
+    console.log(people);
+  }
+  useEffect(() => {
+    callApi();
+  }, []);
+  return <div>HI, I'm app</div>;
 }
 
 export default App;
